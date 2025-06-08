4178a7d682a0074a6e13ba85c4b8a80510a3a866
rysss11 <talanipanabilah@gmail.com>
2025-06-08 23:34:18 +0700

    Add files via upload

---
+++ dpbo_bimble-master/src/main/java/dpbo/bimble/Notification.java
@@ -0,0 +1,93 @@
+package dpbo.bimble;
+
+import java.util.Date;
+
+public abstract class Notification {
+    private String notificationID;
+    private String message;
+    private String type;
+    private Registrasi registration;
+    private String paymentStatus;
+    private Schedule schedule;
+    private Date date;
+    private String Subject;
+
+    public Notification(String notificationID, String message, String type,
+                        Registrasi registration, String paymentStatus, Schedule schedule, Date date, String subject) {
+        this.notificationID = notificationID;
+        this.message = message;	
+        this.type = type;
+        this.registration = registration;
+        this.paymentStatus = paymentStatus;
+        this.schedule = schedule;
+        this.date = date;
+        this.Subject = Subject;
+    }
+
+    // Getter & Setter (Encapsulation)
+    public String getNotificationID() {
+        return notificationID;
+    }
+
+    public void setNotificationID(String notificationID) {
+        this.notificationID = notificationID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Registrasi getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(Registrasi registration) {
+        this.registration = registration;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setTestSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+    
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public String getSubject() {
+    	return Subject;
+    }
+    
+    public void setSubject(String Subject) {
+    	this.Subject = Subject;
+    }
+
+    public abstract String getNotificationDetails();
+}
